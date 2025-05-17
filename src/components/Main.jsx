023ea3ecc7e0722c6a46174079fd0a67e76c5485
JaleelMohammed <iamjaleelmohammed@gmail.com>
2025-05-17 15:42:53 +0530

    nav-bar components

---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+// import Navbar from './Navbar'
+import './Main.css'
+
+export default function Main() {
+  return (
+    <div>
+        {/* <Navbar/> */}
+        <section>
+        
+                <strong>Kids Learning Center</strong>
+        
+                <main class="main">
+                  <img src={Img} />
+                  <h2>New Approach to <br /> kids Education</h2>
+                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita, sit iure. Unde iste nostrum repudiandae laboriosam facilis? Sunt, est eum provident fugiat eos placeat, consequatur vero tenetur a eaque ipsa deserunt nostrum repellat nobis nemo optio enim rem voluptate perspiciatis similique maxime reprehenderit voluptatum molestiae! Reiciendis dolore nihil fuga esse!</p>
+                </main>
+                <button>Learn More</button>
+        
+              </section>
+    </div>
+  )
+}
